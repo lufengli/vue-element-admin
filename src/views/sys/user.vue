430e963a729d73d799590f5a4d44b68fe9639e59
lufeng.li <lufeng.li@icloud.com>
2017-09-27 22:29:23 +0800

    add user

--- src/views/sys/user.vue
+++ src/views/sys/user.vue
@@ -81,52 +81,25 @@
       </el-pagination>
     </div>
     <div>
-      <el-dialog title="新增" :visible.sync="dialogFormVisible">
+      <el-dialog title="新增用户" :visible.sync="dialogFormVisible">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <el-form-item label="活动名称" prop="name">
-            <el-input v-model="ruleForm.name"></el-input>
+          <el-form-item label="登录名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
           </el-form-item>
-          <el-form-item label="活动区域" prop="region">
-            <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="ruleForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="mobile">
+            <el-input v-model="ruleForm.mobile"></el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="status">
+            <el-select v-model="ruleForm.status" placeholder="请选择">
+              <el-option label="启用" value="true"></el-option>
+              <el-option label="禁用" value="false"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="活动时间" required>
-            <el-col :span="11">
-              <el-form-item prop="date1">
-                <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-              <el-form-item prop="date2">
-                <el-time-picker type="fixed-time" placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-              </el-form-item>
-            </el-col>
-          </el-form-item>
-          <el-form-item label="即时配送" prop="delivery">
-            <el-switch on-text="" off-text="" v-model="ruleForm.delivery"></el-switch>
-          </el-form-item>
-          <el-form-item label="活动性质" prop="type">
-            <el-checkbox-group v-model="ruleForm.type">
-              <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-              <el-checkbox label="地推活动" name="type"></el-checkbox>
-              <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-              <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-            </el-checkbox-group>
-          </el-form-item>
-          <el-form-item label="特殊资源" prop="resource">
-            <el-radio-group v-model="ruleForm.resource">
-              <el-radio label="线上品牌商赞助"></el-radio>
-              <el-radio label="线下场地免费"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="活动形式" prop="desc">
-            <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-          </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="submitAddForm('ruleForm')">立即创建</el-button>
+            <el-button type="primary" @click="submitAddForm('ruleForm')">提交</el-button>
             <el-button @click="resetAddForm('ruleForm')">重置</el-button>
           </el-form-item>
         </el-form>
@@ -161,7 +134,7 @@
 </style>
 
 <script>
-  import {getAllSysUsers, getSysUsers} from '@/api/user'
+  import { getSysUsers } from '@/api/user'
   export default {
     data() {
       return {
@@ -184,37 +157,25 @@
         },
         dialogFormVisible: false,
         ruleForm: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          username: '',
+          email: '',
+          mobile: '',
+          status: ''
         },
         rules: {
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          username: [
+            { required: true, message: '请输入登录名', trigger: 'blur' },
+            { min: 6, max: 30, message: '长度在 6 到 30 个字符', trigger: 'blur' }
           ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
+          status: [
+            { required: true, message: '请选择状态', trigger: 'change' }
           ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+          email: [
+            { required: true, message: '请填写正确的邮箱地址', trigger: 'blur' }
           ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
+          mobile: [
+            { required: true, message: '请填写正确的手机号', trigger: 'blur' },
+            { min: 11, max: 11, message: '请填写正确的11位手机号', trigger: 'blur' }
           ]
         }
       }
@@ -234,37 +195,37 @@
         })
       },
       handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
+        console.log(`每页 ${val} 条`)
         this.listQuery.limit = val
         this.getList()
       },
       handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
+        console.log(`当前页: ${val}`)
         this.listQuery.page = val
         this.getList()
       },
       submitSelectForm() {
-        console.log('submit!');
+        console.log('submit!')
         this.listQuery.page = 1
-        this.listQuery.username = this.formInline.username == '' ? null : this.formInline.username
-        this.listQuery.mobile = this.formInline.mobile == '' ? null : this.formInline.mobile
-        this.listQuery.email = this.formInline.email == '' ? null : this.formInline.email
-        this.listQuery.status = this.formInline.status == '' ? null : this.formInline.status
-        console.log(this.formInline);
+        this.listQuery.username = this.formInline.username === '' ? null : this.formInline.username
+        this.listQuery.mobile = this.formInline.mobile === '' ? null : this.formInline.mobile
+        this.listQuery.email = this.formInline.email === '' ? null : this.formInline.email
+        this.listQuery.status = this.formInline.status === '' ? null : this.formInline.status
+        console.log(this.formInline)
         this.getList()
       },
       submitAddForm(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            alert('submit!');
+            alert('submit!')
           } else {
-            console.log('error submit!!');
-            return false;
+            console.log('error submit!!')
+            return false
           }
-        });
+        })
       },
       resetAddForm(formName) {
-        this.$refs[formName].resetFields();
+        this.$refs[formName].resetFields()
       }
     }
   }
