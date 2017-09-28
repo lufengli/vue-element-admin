34db0fea8a87785a5387737c185cb92621183efe
lufeng.li <403666704@qq.com>
2017-09-28 15:06:04 +0800

    代码优化

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 import Qs from 'qs'
@@ -29,45 +29,45 @@ service.interceptors.request.use(config => {
   Promise.reject(error)
 })
 
-// respone拦截器
-service.interceptors.response.use(
-  response => response,
-  /**
-  * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
-  * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
-  */
-//  const res = response.data;
-//     if (res.code !== 20000) {
-//       Message({
-//         message: res.message,
-//         type: 'error',
-//         duration: 5 * 1000
-//       });
-//       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-//         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-//           confirmButtonText: '重新登录',
-//           cancelButtonText: '取消',
-//           type: 'warning'
-//         }).then(() => {
-//           store.dispatch('FedLogOut').then(() => {
-//             location.reload();// 为了重新实例化vue-router对象 避免bug
-//           });
-//         })
-//       }
-//       return Promise.reject('error');
-//     } else {
-//       return response.data;
-//     }
-  error => {
-    console.log('err' + error)// for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
+// 添加响应拦截器
+service.interceptors.response.use(response => {
+  // 对响应数据做点什么
+  console.log('正常响应数据--------------')
+  console.log(response)
+  if (!response.data.success) {
+    if (response.data.code === 12002) {
+      MessageBox.confirm('你已被登出,可以取消继续留在该页面，或者重新登录?', '确定登出', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        store.dispatch('FedLogOut').then(() => {
+          location.reload() // 为了重新实例化vue-router对象 避免bug
+        })
+      })
+    } else if (response.data.code === 10001 || response.data.code === 10002 || response.data.code === 10003 || response.data.code === 11001) {
+      store.dispatch('FedLogOut').then(() => {
+        location.reload() // 为了重新实例化vue-router对象 避免bug
+      })
+      Message({
+        message: '未登录或登录已过期,请重新登录！',
+        type: 'error',
+        duration: 10 * 1000
+      })
+    }
+    return Promise.reject('error')
   }
-)
+  return response
+}, error => {
+  // 对响应错误做点什么
+  console.log('错误响应数据----------------')
+  console.log(error)
+  Message({
+    message: '抱歉！系统异常，请刷新页面或联系管理员，谢谢。',
+    type: 'error',
+    duration: 5 * 1000
+  })
+  return Promise.reject(error)
+})
 
 export default service
